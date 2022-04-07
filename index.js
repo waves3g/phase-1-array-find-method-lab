5720e92c70008d525f633ae306df9a673c6e97b8
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-07 09:58:20 -0700

    commit tree a78edc5bb83e1e54dd154966312ac0ea1e1899c4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    let answer = record.find(function(game){
+       return game.result === "W" 
+    });
+    if (answer) {
+        return answer.year;
+    }
+}
